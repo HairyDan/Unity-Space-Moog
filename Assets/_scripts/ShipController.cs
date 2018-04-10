1d463efae5bf5dc4f4e1f2c27d50036e92e3436a
Daniel Schormans <danielschormans@googlemail.com>
2018-04-10 22:17:08 +0100

    added project

---
+++ Assets/_scripts/ShipController.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public float twistSpeed;
+    public float xBound, zBound;
+
+    public ParticleSystem trail;
+
+    private bool colliding;
+
+    public GameObject bullet;
+    private GameObject bulletEjector;
+
+    public bool currentlyDestroyed;
+
+    public GameObject deathExplosion;
+
+    public AudioSource laserPew;
+    public AudioSource deathSound;
+
+    public GameObject spawnpoint1, spawnpoint2, spawnpoint3, spawnpoint4;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        bulletEjector = GameObject.FindWithTag("redBulletEjector");
+        trail.Stop();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        colliding = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        colliding = false;
+    }
+
+    private void createAndFireBullet()
+    {
+        AudioSource pew = Instantiate(laserPew);
+        Destroy(pew.gameObject, 0.5f);
+        GameObject instanceBullet = Instantiate(bullet, bulletEjector.transform.position, transform.rotation) as GameObject;
+        Rigidbody laserBullet = instanceBullet.GetComponent<Rigidbody>();
+        laserBullet.AddRelativeForce(new Vector3(0.0f, -1300.0f, 0.0f));
+    }
+
+    public void shipDestroyed()
+    {
+        AudioSource boomSound = Instantiate(deathSound);
+        Destroy(boomSound.gameObject, 2.0f);
+        currentlyDestroyed = true;
+        GameObject boom = Instantiate(deathExplosion, transform.position, Quaternion.Euler(0, 0, 0));
+        Destroy(boom, 1);
+        //don't actually destroy ship, just hide to save resources
+        transform.position = new Vector3(500f, 500f, 500f);
+        
+    }
+
+    public void shipRespawn()
+    {
+        currentlyDestroyed = false;
+        int seed = Random.Range(1, 5);
+        if (seed == 1)
+        {
+            transform.position = spawnpoint1.transform.position;
+            transform.rotation = spawnpoint1.transform.rotation;
+        }
+        if (seed == 2)
+        {
+            transform.position = spawnpoint2.transform.position;
+            transform.rotation = spawnpoint2.transform.rotation;
+        }
+        if (seed == 3)
+        {
+            transform.position = spawnpoint3.transform.position;
+            transform.rotation = spawnpoint3.transform.rotation;
+        }
+        if (seed == 4)
+        {
+            transform.position = spawnpoint4.transform.position;
+            transform.rotation = spawnpoint4.transform.rotation;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (!currentlyDestroyed)
+        {
+
+            if (Input.GetButtonDown("RedFire"))
+            {
+                createAndFireBullet();
+            }
+
+            if (Input.GetButtonDown("Vertical"))
+            {
+                trail.Play();
+            }
+            if (Input.GetButtonUp("Vertical"))
+            {
+                trail.Stop();
+            }
+
+            float twistLeftRight = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 twistVect = new Vector3(0.0f, twistLeftRight * twistSpeed, 0.0f);
+
+            rb.angularVelocity = twistVect;
+
+            Vector3 forwardBackward = new Vector3(0.0f, -moveVertical, 0.0f);
+
+            rb.AddRelativeForce(forwardBackward * speed);
+
+            rb.position = new Vector3(
+                Mathf.Clamp(rb.position.x, -xBound, xBound), 0, Mathf.Clamp(rb.position.z, -zBound, zBound)
+                );
+
+            float currenty = rb.rotation.y;
+
+            if (!colliding)
+            {
+                Quaternion rotationFrom = rb.rotation;
+                Quaternion rotationTo = Quaternion.Euler(-90, rb.rotation.eulerAngles.y, 0);
+                rb.rotation = Quaternion.RotateTowards(rotationFrom, rotationTo, Time.deltaTime * 100.0f);
+                // rb.rotation = Quaternion.Euler(-90.0f, rb.rotation.eulerAngles.y, 0.0f);
+            }
+        }
+
+    }
+}
---
+++ Assets/_scripts/tutController.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutController : MonoBehaviour {
+
+    public int rScoreInt, bScoreInt, bhealth, rhealth;
+    public GUIText redScore, blueScore, redHealthDisplay, blueHealthDisplay;
+
+    public GameObject redShip, blueShip, redEnemy, blueEnemy;
+
+    private ShipController redController;
+    private BlueController blueController;
+
+    public GameObject destructWall;
+
+    public AudioSource shipBoom;
+    public GameObject redExplode, blueExplode;
+
+    private bool redEnemyDead, blueEnemyDead;
+
+    public GameObject welcomepanel, territoriespanel, shootingpanel, finishpanel;
+    private bool welcomefreeze, territoryfreeze;
+    private bool blueForward, blueTurn, redForward, redTurn;
+    private bool norefreeze, noshootrefreeze;
+
+
+	// Use this for initialization
+	void Start () {
+        rScoreInt = 0;
+        bScoreInt = 0;
+        bhealth = 3;
+        rhealth = 3;
+
+        redController = redShip.GetComponent<ShipController>();
+        blueController = blueShip.GetComponent<BlueController>();
+
+        redEnemyDead = false;
+        blueEnemyDead = false;
+
+       // redController.currentlyDestroyed = true;
+       // blueController.currentlyDestroyed = true;
+
+        redScore.fontSize = -5;
+        blueScore.fontSize = -5;
+
+        redHealthDisplay.fontSize = -5;
+        blueHealthDisplay.fontSize = -5;
+
+        welcomefreeze = true;
+        territoryfreeze = norefreeze = noshootrefreeze = false;
+        blueForward = blueTurn = redForward = redTurn = false;
+
+        territoriespanel.SetActive(false);
+
+        shootingpanel.SetActive(false);
+
+        finishpanel.SetActive(false);
+
+    }
+
+    public void addScore(int redAdd, int blueAdd)
+    {
+        rScoreInt += redAdd;
+        bScoreInt += blueAdd;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        redScore.text = "Score: \n" + rScoreInt.ToString();
+        blueScore.text = "Score: \n" + bScoreInt.ToString();
+        redHealthDisplay.text =  "Dummy HP:\n"+rhealth.ToString();
+        blueHealthDisplay.text = "Dummy HP:\n"+bhealth.ToString();
+
+        if (redEnemyDead && blueEnemyDead)
+        {
+            finishpanel.SetActive(true);
+            if (Input.GetButtonDown("BlueFire"))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("mainmenu");
+            }
+        }
+
+        if(rScoreInt >= 10 && bScoreInt >= 10 && !noshootrefreeze)
+        {
+            redScore.fontSize = 39;
+            blueScore.fontSize = 39;
+            Destroy(destructWall);
+
+            shootingpanel.SetActive(true);
+
+            redController.currentlyDestroyed = true;
+            blueController.currentlyDestroyed = true;
+
+            if (Input.GetButtonDown("BlueFire") || Input.GetButtonDown("RedFire"))
+            {
+                shootingpanel.SetActive(false);
+                noshootrefreeze = true;
+                redController.currentlyDestroyed = false;
+                blueController.currentlyDestroyed = false;
+
+                redHealthDisplay.fontSize = 39;
+                blueHealthDisplay.fontSize = 39;
+
+            }
+        }
+        
+            if (!blueForward && Input.GetButtonDown("VerticalBlue"))
+            {
+                blueForward = true;
+            }
+            if (!blueTurn && Input.GetButtonDown("HorizontalBlue"))
+            {
+                blueTurn = true;
+            }
+            if (!redForward && Input.GetButtonDown("Vertical"))
+            {
+                redForward = true;
+            }
+            if (!redTurn && Input.GetButtonDown("Horizontal"))
+            {
+                redTurn = true;
+            }
+
+        if (welcomefreeze)
+        {
+            if (welcomefreeze && (blueForward || blueTurn || redForward || redTurn))
+            {
+                welcomefreeze = false;
+                Destroy(welcomepanel);
+            }
+        }
+        if (!welcomefreeze)
+        {
+            if(blueForward && redForward && blueTurn && redTurn && !norefreeze)
+            {
+                redController.currentlyDestroyed = true;
+                blueController.currentlyDestroyed = true;
+
+                territoriespanel.SetActive(true);
+                territoryfreeze = true;
+                norefreeze = true;
+            }
+
+            if (Input.GetButtonDown("BlueFire"))
+            {
+                territoriespanel.SetActive(false);
+                
+            }
+            if (territoryfreeze && Input.GetButtonUp("BlueFire"))
+            {
+                redController.currentlyDestroyed = false;
+                blueController.currentlyDestroyed = false;
+                territoryfreeze = false;
+            }
+        }
+
+    }
+
+    void RoundOver()
+    {
+
+    }
+
+    public void damageDealt(int bDamage, int rDamage)
+    {
+        rhealth -= rDamage;
+        bhealth -= bDamage;
+        if (rhealth <= 0 && redEnemyDead == false)
+        {
+            shipDestroyed(true);
+        }
+        if (bhealth <= 0 && blueEnemyDead == false)
+        {
+            shipDestroyed(false);
+        }
+    }
+
+    public void shipDestroyed(bool isRed)
+    {
+        if (isRed)
+        {
+            AudioSource boomSound = Instantiate(shipBoom);
+            Destroy(boomSound.gameObject, 2.0f);
+            GameObject boom = Instantiate(redExplode, redEnemy.transform.position, Quaternion.Euler(0, 0, 0));
+            Destroy(boom, 1);
+            Destroy(redEnemy);
+            redEnemyDead = true;
+        }
+
+        else
+        {
+            AudioSource boomSound = Instantiate(shipBoom);
+            Destroy(boomSound.gameObject, 2.0f);
+            GameObject boom = Instantiate(blueExplode, blueEnemy.transform.position, Quaternion.Euler(0, 0, 0));
+            Destroy(boom, 1);
+            Destroy(blueEnemy);
+            blueEnemyDead = true;
+        }
+
+    }
+
+}
