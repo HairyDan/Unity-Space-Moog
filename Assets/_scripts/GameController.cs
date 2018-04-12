78dc57a01e2a2961f11e4099ddd2ec7af773891e
Daniel Schormans <danielschormans@googlemail.com>
2018-04-12 20:20:42 +0100

    pushing fixed repo with fixed gitignore

---
+++ Assets/_scripts/GameController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public int rScoreInt, bScoreInt, bhealth, rhealth;
+    public GUIText redScore, blueScore, redHealthDisplay, blueHealthDisplay, roundTimerDisplay;
+
+    public GameObject redShip, blueShip, startPanel, gameoverpanel;
+    public UnityEngine.UI.Text gameoverText;
+
+    private ShipController redController;
+    private BlueController blueController;
+
+    private float roundTimer;
+    private bool gameover;
+
+	// Use this for initialization
+	void Start () {
+        rScoreInt = 0;
+        bScoreInt = 0;
+        bhealth = 3;
+        rhealth = 3;
+
+        redScore.text = "Score: \n" + rScoreInt.ToString();
+        blueScore.text = "Score: \n" + bScoreInt.ToString();
+        redHealthDisplay.text = "HP: " + rhealth.ToString();
+        blueHealthDisplay.text = "HP: " + bhealth.ToString();
+
+        roundTimer = 60;
+        roundTimerDisplay.text = "Time Remaining: " + roundTimer.ToString();
+
+        redController = redShip.GetComponent<ShipController>();
+        blueController = blueShip.GetComponent<BlueController>();
+        gameover = false;
+        gameoverpanel.SetActive(false);
+    }
+
+    public void addScore(int redAdd, int blueAdd)
+    {
+        rScoreInt += redAdd;
+        bScoreInt += blueAdd;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (startPanel.activeSelf)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPanel.SetActive(false);
+            }
+        }
+        if (!startPanel.activeSelf && !gameover)
+        {
+            roundTimer -= Time.deltaTime;
+            roundTimerDisplay.text = "Time Remaining: " + Mathf.Round(roundTimer).ToString();
+            if (roundTimer < 0)
+            {
+                RoundOver();
+            }
+
+
+            redScore.text = "Score: \n" + rScoreInt.ToString();
+            blueScore.text = "Score: \n" + bScoreInt.ToString();
+            redHealthDisplay.text = "HP: " + rhealth.ToString();
+            blueHealthDisplay.text = "HP: " + bhealth.ToString();
+        }
+        if (gameover)
+        {
+            if (Input.GetButtonDown("Submit"))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("mainmenu");
+            }
+        }
+    }
+
+    void RoundOver()
+    {
+        gameover = true;
+        redController.currentlyDestroyed = true;
+        blueController.currentlyDestroyed = true;
+
+        string winner;
+        string winnerpoints;
+        if (rScoreInt >= bScoreInt)
+        {
+            winner = "Red";
+            winnerpoints = rScoreInt.ToString();
+        }
+        else {
+            winnerpoints = bScoreInt.ToString();
+            winner = "Blue";
+        }
+
+        gameoverText.text = "The Winner is " +winner +" with "+ winnerpoints + " points! \n Press Enter to return to the main menu.";
+
+        gameoverpanel.SetActive(true);
+
+
+    }
+
+    public void damageDealt(int rDamage, int bDamage)
+    {
+        rhealth -= rDamage;
+        bhealth -= bDamage;
+        if (rhealth <= 0)
+        {
+            redController.shipDestroyed();
+            respawnShip(true);
+            rhealth = 3;
+        }
+        if (bhealth <= 0)
+        {
+            blueController.shipDestroyed();
+            respawnShip(false);
+            bhealth = 3;
+        }
+    }
+
+    public void respawnShip(bool isRed)
+    {
+        if (isRed)
+        {
+            StartCoroutine(respawnTimer(isRed));
+        }
+        else
+        {
+            StartCoroutine(respawnTimer(isRed));
+        }
+    }
+
+    private IEnumerator respawnTimer(bool red)
+    {
+        yield return new WaitForSeconds(3);
+        if (red)
+        {
+            redController.shipRespawn();
+        } else
+        {
+            blueController.shipRespawn();
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Unity-Space-Moog
+
+AKA 2 Player Asteroids.
+
+This is a cheesy space shooter for 2 players on 1 keyboard.
+
+There are a lot of placeholder graphics, especially UI and Terrain (fairly happy with particle effects currently).
+
+The game is currently fairly bare-bones but quite fun for a few quick games with a friend.
